5be5d4bcc5f57e00fd7c592272d8c584b2969352
movlan17 <movlanomerov00@gmail.com>
2025-11-30 16:58:53 +0400

    fsd

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,3 +1,36 @@
-O(n)
-O(n^2)
-O(n^2)
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ *               using the Bubble sort algorithm
+ * @array: pointer to array
+ * @size: size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, j;
+    int tmp;
+    int swapped;
+
+    if (array == NULL || size < 2)
+        return;
+
+    for (i = 0; i < size - 1; i++)
+    {
+        swapped = 0;
+        for (j = 0; j < size - i - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+                swapped = 1;
+                print_array(array, size);
+            }
+        }
+        if (!swapped)
+            break;
+    }
+}
+
