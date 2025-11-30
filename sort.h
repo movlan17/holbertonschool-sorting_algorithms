be52a0a7d47ef7aa3668d061c3caa96b7a7f1d8b
movlan17 <movlanomerov00@gmail.com>
2025-11-30 17:21:08 +0400

    fds

--- sort.h
+++ sort.h
@@ -6,6 +6,7 @@
 /* Function prototypes */
 void print_array(const int *array, size_t size);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif /* SORT_H */
 
