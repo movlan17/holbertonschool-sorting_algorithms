b7d6e13822bf53c9ff7f7052c406f4714d5a9541
movlan17 <movlanomerov00@gmail.com>
2025-11-30 17:11:45 +0400

    sdf

--- sort.h
+++ sort.h
@@ -8,8 +8,8 @@
  * struct listint_s - Doubly linked list node
  *
  * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
+ * @prev: Pointer to the previous element
+ * @next: Pointer to the next element
  */
 typedef struct listint_s
 {
@@ -22,9 +22,15 @@ typedef struct listint_s
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 
-/* Sorting prototypes */
+/* Sorting algorithms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 
+/* Helper functions used by insertion sort */
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
+
+/* Helper functions for testing in main_0.c */
+listint_t *create_listint(const int *array, size_t size);
+
 #endif /* SORT_H */
-/
+
